23bf7a10e2d726b0b58296cbb112adacfb3d69d3
DhondtTiemen <tiemen.dhondt2@student.howest.be>
2022-09-21 15:35:12 +0200

    Add Navbar

--- pwa/src/App.vue
+++ pwa/src/App.vue
@@ -1,15 +1,17 @@
 <template>
+  <app-header></app-header>
   <router-view></router-view>
 </template>
 
 <script lang="ts">
 import useFirebase from './composables/useFirebase'
+import AppHeader from './components/generic/AppHeader.vue';
 
 export default {
-  setup() {
-    const { app } = useFirebase()
-
-    return {}
-  },
+    setup() {
+        const { app } = useFirebase();
+        return {};
+    },
+    components: { AppHeader }
 }
 </script>
---
+++ pwa/src/components/generic/AppHeader.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="flex justify-between h-24 items-center">
+        <div class="w-16 h-16 mt-4 ml-4 hover:animate-spin">
+            <router-link to="/">
+                <Logo class="w-max h-max"/>
+            </router-link>
+        </div>
+        <div class="flex justify-end w-full gap-12 mr-8 font-bold">
+            <p class="hover:text-neutral-600">
+                <router-link to="/">
+                    Home
+                </router-link>
+            </p>
+            <p class="hover:text-neutral-600">
+                <router-link to="/birds">
+                    Vogels
+                </router-link>
+            </p>
+            <p class="hover:text-neutral-600">
+                <router-link to="/log">
+                    Logboek
+                </router-link>
+            </p>
+            <p class="hover:text-neutral-600">
+                <router-link to="/observations">
+                    Observaties
+                </router-link>
+            </p>
+            <p class="hover:text-neutral-600">
+                <router-link to="/account">
+                    {{ user?.displayName }}
+                </router-link>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import useAuthentication from '../../composables/useAuthentication';
+    import Logo from './logo.vue';
+
+    export default {
+    setup() {
+        const { user } = useAuthentication();
+        return { user, Logo };
+    },
+    components: {
+        Logo
+    }
+}
+</script>
