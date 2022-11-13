e811fe64b3a4f27824f7136fe9b948e79c03113d
Christophe Laprudence <32491012+christophe-laprudence-howest@users.noreply.github.com>
2022-11-13 11:49:25 +0100

    feat: validation Location graphql

--- api/src/locations/entities/area.entity.ts
+++ api/src/locations/entities/area.entity.ts
@@ -1,11 +1,24 @@
 import { Field, InputType, ObjectType } from '@nestjs/graphql'
+import {
+  IsNotEmpty,
+  ValidateNested,
+  Equals,
+  IsArray,
+  ArrayMinSize,
+  ArrayNotEmpty,
+} from 'class-validator'
 
 @InputType('AreaInput')
 @ObjectType()
 export class Area {
+  @IsArray()
+  @ArrayMinSize(1)
+  @ArrayNotEmpty()
   @Field(() => [[[Number]]])
   coordinates: number[][][]
 
+  @IsNotEmpty()
+  @Equals('Polygon')
   @Field()
   type: string
 }
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,11 @@
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const port: number = +process.env.PORT || 3003
-
+  app.useGlobalPipes(new ValidationPipe())
   await app.listen(port)
 
   console.info(
