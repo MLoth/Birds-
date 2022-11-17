9d9e234e800646154f7bb024e17b2b6a8a45090d
Martijn Loth <MLoth@users.noreply.github.com>
2022-11-17 13:50:05 +0100

    fix: throw error when env is not set

--- pwa/cypress/e2e/1-auth-flow/register.cy.ts
+++ pwa/cypress/e2e/1-auth-flow/register.cy.ts
@@ -1,3 +1,35 @@
+import {
+  Auth,
+  connectAuthEmulator,
+  initializeAuth,
+  signInWithEmailAndPassword,
+  UserCredential,
+  indexedDBLocalPersistence,
+} from 'firebase/auth'
+
+function getAuthEmulatorHost() {
+  const host = Cypress.env('NEXT_PUBLIC_FIREBASE_EMULATOR_HOST') as string
+  const port = Cypress.env('NEXT_PUBLIC_FIREBASE_AUTH_EMULATOR_PORT') as string
+
+  return ['http://', host, ':', port].join('')
+}
+
+let auth: Auth
+
+function getAuth() {
+  const app = createFirebaseApp()
+
+  auth =
+    auth ||
+    initializeAuth(app, {
+      persistence: indexedDBLocalPersistence,
+    })
+
+  connectAuthEmulator(auth, getAuthEmulatorHost())
+
+  return auth
+}
+
 describe('go to register', () => {
   it('should find register', () => {
     cy.visit('http://localhost:5173/auth/register')
