e36f6f182d540526dc5e1897882da498913344f5
Martijn Loth <MLoth@users.noreply.github.com>
2022-11-18 09:22:10 +0100

    chore: minor clean-up

--- pwa/src/screens/Home.vue
+++ pwa/src/screens/Home.vue
@@ -1,11 +1,6 @@
 <template>
   <route-holder title="Home">
     <div class="mb-6">
-      <p>{{ $t('message.hello') }}</p>
-      <p>{{ $t('test') }}</p>
-    </div>
-
-    <div class="mb-6">
       <h2 class="font-theme mb-3 text-2xl font-medium tracking-wide">
         Random birds
       </h2>
